782730be7a50f05cf0c7ddee56280bcd23e41f42
Aman Dhiman <amandhiman251@gmail.com>
2020-10-17 23:55:47 +0530

    In App.js the component changed from App class to App function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 import './UserOutput/UserOutput.css';
 
-class App extends Component {
-  state = (
+const App =() => {
+  const state = (
     { username: "Aman Dhiman"}
   )
-  userChangeHandler = (event) => {
+ const userChangeHandler = (event) => {
     this.setState( {
       username: event.target.value
     });
   }
    
-  render() {
-    const myStyle = {
-      width: "176px",
-      margin: "20px auto",
-      border: "2px solid #eee",
-      boxShadow: "0 2px 2px #ccc"
-    };
-    return (
-      <div className="App">
-        <UserInput style = {myStyle} change = {this.userChangeHandler} username = {this.state.username} />
-        <UserOutput username = {this.state.username} />
-        <UserOutput username = {this.state.username} />
-        <UserOutput username = {this.state.username} />
-        <UserOutput username = {this.state.username} />
-      </div>
-    );
-  }
+  const myStyle = {
+    width: "176px",
+    margin: "20px auto",
+    border: "2px solid #eee",
+    boxShadow: "0 2px 2px #ccc"
+  };
+  return (
+    <div className="App">
+      <UserInput style = {myStyle} change = {userChangeHandler} username = {state.username} />
+      <UserOutput username = {state.username} />
+      <UserOutput username = {state.username} />
+      <UserOutput username = {state.username} />
+      <UserOutput username = {state.username} />
+    </div>
+  );
 }
 
 
